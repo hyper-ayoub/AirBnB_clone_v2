ca69898642e36917abc54c81ad931dad885212a2
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-01 20:55:19 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,32 @@
-#!/usr/bin/python3
-""" document documt """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import unittest
 
 
-class test_review(test_basemodel):
-    """ document documt """
+class TestReview(test_basemodel):
+    """ Test Review class """
 
     def __init__(self, *args, **kwargs):
-        """ document documt """
+        """ Initialize test Review class """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ document documt """
+        """ Test place_id attribute """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ document documt """
+        """ Test user_id attribute """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ document documt """
+        """ Test text attribute """
         new = self.value()
         self.assertEqual(type(new.text), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
