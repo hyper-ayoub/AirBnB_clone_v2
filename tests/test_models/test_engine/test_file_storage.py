1326ba76ea801af30f7256cbd1aa9bdd92fb0f16
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-10 00:41:27 +0100

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module for testing file -> storage"""
+""" Module for testing file storage"""
 import unittest
 from models.base_model import BaseModel
 from models import storage
