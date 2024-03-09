5f2547f3a2467aee1849e2fac5d590def87af404
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-10 00:08:46 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,28 +1,24 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
+#!/usr/bin/python
+""" holds class Review"""
+import models
 from models.base_model import BaseModel, Base
-# SQLAlchemy modules
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Defines a class Review
-
-    Attributes:
-        __tablename__ (str): reviews
-
-        place_id (string): id of place.
-        user_id (string): id of user.
-        text (string): just a text.
-    """
-    __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes Review instance"""
+        """initializes Review"""
         super().__init__(*args, **kwargs)
-        self.place_id = str(self.place_id)
-        self.user_id = str(self.user_id)
-        self.text = str(self.text)
