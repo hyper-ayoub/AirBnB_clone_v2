5bad0f4a348b67b58d075d439185058512777d25
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-10 00:25:51 +0100

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" document documt """
+"""Test module for the Amenity class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import unittest
 
 
 class test_Amenity(test_basemodel):
-    """ document documt """
+    """Test class for Amenity"""
 
-    def __init__(self, *args, **kwargs):
-        """ document documt """
-        super().__init__(*args, **kwargs)
+    def setUp(self):
+        """Set up for test"""
+        super().setUp()
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ document documt """
-        new = self.value()
+        """Test the name attribute of Amenity"""
+        new = self.value(name="name")
         self.assertEqual(type(new.name), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
